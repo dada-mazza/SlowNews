a8ddf5ed3bfbf4d151327feee8e2450c9b238854
dada.mazza <dada.mazza@gmail.com>
2015-12-13 14:29:43 +0200

    add file

--- src/main/java/slownews/moxy/package-info.java
+++
@@ -1,9 +0,0 @@
-@XmlSchema(
-        elementFormDefault = XmlNsForm.QUALIFIED,
-        xmlns = {@XmlNs(prefix = "media", namespaceURI = "http://search.yahoo.com/mrss/")}
-)
-package slownews.moxy;
-
-import javax.xml.bind.annotation.XmlNs;
-import javax.xml.bind.annotation.XmlNsForm;
-import javax.xml.bind.annotation.XmlSchema;
